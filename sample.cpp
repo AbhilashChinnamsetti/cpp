33d1e9a06eb5f1e3ba995b1597a07048047bb8c2
AbhilashChinnamsetti <146182264+AbhilashChinnamsetti@users.noreply.github.com>
2023-09-28 08:08:36 +0530

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    string s1;
+    getline(cin,s1);
+    cin >>x;
+    cout <<"the value of x is:"<<x<<"\n";
+    cout<<s1;
+    return 0;
+
+}
